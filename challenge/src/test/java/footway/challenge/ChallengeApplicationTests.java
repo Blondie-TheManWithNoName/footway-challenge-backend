8acbb55fa6707fcc911f4212589421387d55d57b
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-11-27 19:51:29 +0100

    init

---
+++ challenge/src/test/java/footway/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package footway.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
