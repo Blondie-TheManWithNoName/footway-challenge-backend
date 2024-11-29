d8aa719272d3a88e4664cf0855a6670b2c4d8c5d
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-11-29 20:42:18 +0100

    create repos

--- challenge/src/main/java/footway/challenge/ChallengeApplication.java
+++ challenge/src/main/java/footway/challenge/ChallengeApplication.java
@@ -2,8 +2,10 @@ package footway.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "footway.challenge.repos")
 public class ChallengeApplication {
 
 	public static void main(String[] args) {
