55a03ce3b6430460d281a93c8fb1d23ceaf52ccf
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-12-11 01:43:59 +0100

    update products entities

--- challenge/src/main/java/footway/challenge/entities/DigitalProduct.java
+++ challenge/src/main/java/footway/challenge/entities/DigitalProduct.java
@@ -1,25 +1,120 @@
-
 package footway.challenge.entities;
 
-import java.util.Set;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "digital_product")
-public class DigitalProduct extends Product {
+@Table(name = "digital_product", indexes = {
+        @Index(name = "digital_product_ean", columnList = "EAN", unique = true),
+// @Index(name = "digital_product_name_description_fulltext_idx", columnList =
+// "name, description", unique = false)
+// CREATE FULLTEXT INDEX digital_name_description_fulltext_idx ON
+// digital_product(name, description);
 
-    @ManyToMany(mappedBy = "digitalProducts")
-    private Set<PhysicalProduct> physicalProducts;
+})
+public class DigitalProduct {
+
+    // * SKU Number */
+    @Id
+    private String sku;
+
+    // * EAN Number */
+    @Column(nullable = true)
+    private String EAN;
+
+    // * Name */
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String name;
+
+    // * Description */
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String description;
+
+    // * Price */
+    @Column(nullable = false)
+    private double price;
+
+    // * Variant Color */
+    @Column(nullable = false)
+    private String color;
+
+    // * Variant Size */
+    @Column(nullable = false)
+    private int size;
+
+    // * Constructor */
+    public DigitalProduct(String EAN, String SKU, String name, String description,
+            double price, String color, int size) {
+        this.EAN = EAN;
+        this.sku = SKU;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.color = color;
+        this.size = size;
+    }
 
     public DigitalProduct() {
     }
 
-    public DigitalProduct(String EAN, String SKU, String name, String description, double price, String color,
-            int size) {
-        super(EAN, SKU, name, description, price, color, size);
+    // * GETTERS & SETTERS */
+
+    public String getSku() {
+        return sku;
     }
 
+    public void setSku(String SKU) {
+        this.sku = SKU;
+    }
+
+    public String getEAN() {
+        return EAN;
+    }
+
+    public void setEAN(String EAN) {
+        this.EAN = EAN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
--- challenge/src/main/java/footway/challenge/entities/PhysicalProduct.java
+++ challenge/src/main/java/footway/challenge/entities/PhysicalProduct.java
@@ -1,27 +1,132 @@
 package footway.challenge.entities;
 
-import java.util.Set;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
-@Table(name = "physical_product")
-public class PhysicalProduct extends Product {
+@Table(name = "physical_product", indexes = {
+        // @Index(name = "physical_product_sku", columnList = "sku", unique = true),
+        @Index(name = "physical_product_ean", columnList = "EAN", unique = true),
+// @Index(name = "physical_product_name_description_fulltext_idx", columnList =
+// "name, description", unique = false)
+// CREATE FULLTEXT INDEX name_description_fulltext_idx ON physical_product(name,
+// description);
+})
+public class PhysicalProduct {
 
-    @ManyToMany
-    @JoinTable(name = "physical_digital_map", joinColumns = @JoinColumn(name = "physical_product_id"), inverseJoinColumns = @JoinColumn(name = "digital_product_id"))
-    private Set<DigitalProduct> digitalProducts;
+    // * SKU Number */
+    @Id
+    private String sku;
+
+    // * EAN Number */
+    @Column(nullable = true)
+    private String EAN;
+
+    // * Name */
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String name;
+
+    // * Description */
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String description;
+
+    // * Price */
+    @Column(nullable = false)
+    private double price;
+
+    // * Variant Color */
+    @Column(nullable = false)
+    private String color;
+
+    // * Variant Size */
+    @Column(nullable = false)
+    private int size;
+
+    @OneToMany(mappedBy = "physicalProduct")
+    @JsonBackReference
+    private List<Mappings> mappings;
+
+    // * Constructor */
+    public PhysicalProduct(String EAN, String SKU, String name, String description,
+            double price, String color, int size) {
+        this.EAN = EAN;
+        this.sku = SKU;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.color = color;
+        this.size = size;
+    }
 
     public PhysicalProduct() {
     }
 
-    public PhysicalProduct(String EAN, String SKU, String name, String description, double price, String color,
-            int size) {
-        super(EAN, SKU, name, description, price, color, size);
+    // * GETTERS & SETTERS */
+
+    public String getSku() {
+        return sku;
     }
 
+    public void setSku(String SKU) {
+        this.sku = SKU;
+    }
+
+    public String getEAN() {
+        return EAN;
+    }
+
+    public void setEAN(String EAN) {
+        this.EAN = EAN;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public List<Mappings> getMappings() {
+        return mappings;
+    }
 }
